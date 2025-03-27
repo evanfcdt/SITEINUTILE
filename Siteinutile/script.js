c8e5673d2a2582f3ac3c695642f6975bb688c76f
evanfcdt <evanfourcaudot46@gmail.com>
2025-03-27 22:32:40 +0100

    Add files via upload

---
+++ Siteinutile/script.js
@@ -0,0 +1,4 @@
+function direHello() {
+    const messageElement = document.getElementById("message");
+    messageElement.textContent = "Hello, vous avez cliqué sur le bouton le plus inutile de tous les temps !";
+}
